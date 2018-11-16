9fe44167769df1535095c7a33836364b32c507c0
Vagrant <vagrant@AARON.qac.local>
2018-11-16 04:34:09 -0600

    First commit

---
+++ Calc.java
@@ -0,0 +1,10 @@
+public class Calc { 
+	public static void main(String args[]) {
+		int input1 = 5;
+		int input2 = 2;
+		System.out.println(input1 + input2);
+		System.out.println(input1 - input2);
+		System.out.println(input1 * input2);
+		System.out.println(input1 / input2);
+	}
+}
